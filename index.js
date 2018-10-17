09d629f21666d07acad0c51438e2a7d1636d6bfd
ryanholly3 <ryanholly3@gmail.com>
2018-10-17 12:02:19 -0600

    added people.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const people = require('./people')
 
 app.use(cors());
 app.use(bodyParser());
 
 
 app.get('/', (req, res, next)=>{
-  res.send('HELLO')
+  res.json(people)
 })
 
 app.listen(port, () => {
