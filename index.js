1fd3887b326efec0190942139016e98119ae0d57
ryanholly3 <ryanholly3@gmail.com>
2018-10-17 13:01:26 -0600

    added error

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ const port = 3000;
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const people = require('./people')
+const error = require('./error')
 
 app.use(cors());
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 
 app.get('/', (req, res, next)=>{
   res.json(people)
+})
+
+people.map((person, i)=>{
+  return app.get(`/${person.id}`, (req, res, next)=>{
+    res.json(people[i])
+  })
+})
+
+app.get('/*', (req, res, next)=>{
+  res.json(error)
 })
 
 app.listen(port, () => {
--- people.js
+++ people.js
@@ -1,21 +1,21 @@
 module.exports = [{
-  id: '1',
-  cohortName: '17-01-WD-DP',
-  cohortCode: 'g100',
-  numberOfStudents: '28'
-}, {
-  id: '2',
-  cohortName: '17-01-DS-GT',
-  cohortCode: 'g105',
-  numberOfStudents: '24'
-}, {
-  id: '3',
-  cohortName: '17-01-WD-PX',
-  cohortCode: 'g109',
-  numberOfStudents: '30'
-}, {
-  id: '4',
-  cohortName: '17-01-WD-BD',
-  cohortCode: 'g110',
-  numberOfStudents: '29'
-}]
+    id: '1',
+    cohortName: '17-01-WD-DP',
+    cohortCode: 'g100',
+    numberOfStudents: '28'
+  }, {
+    id: '2',
+    cohortName: '17-01-DS-GT',
+    cohortCode: 'g105',
+    numberOfStudents: '24'
+  }, {
+    id: '3',
+    cohortName: '17-01-WD-PX',
+    cohortCode: 'g109',
+    numberOfStudents: '30'
+  }, {
+    id: '4',
+    cohortName: '17-01-WD-BD',
+    cohortCode: 'g110',
+    numberOfStudents: '29'
+  }]
